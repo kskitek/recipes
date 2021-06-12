d65f5a5092d276fdc6b7ea4c603d95e4e70f1809
Krzysztof Skitek <krzysztof.skitek@gmail.com>
2021-06-12 11:32:00 +0200

    impl: Render recipe

--- src/Recipe.js
+++ src/Recipe.js
@@ -10,9 +10,41 @@ function Recipe(props) {
   return (
     <div>
       {error && error}
-      {recipe && JSON.stringify(recipe)}
+      {recipe && <Details recipe={recipe}/>}
     </div>
   )
 }
+
+function Details(props) {
+  const recipe = props.recipe;
+  return (
+    <>
+      <p className="title">{recipe.name}</p>
+      <a href={recipe.url}>{recipe.url}</a>
+      <Ingridients ingridients={recipe.ingridients}/>
+      <p className="description">{recipe.description}</p>
+    </>
+  );
+}
+
+// TODO ingridient.name should not be a key
+
+function Ingridients(props) {
+  const ingridients = props.ingridients.map(i => <Ingridient key={i.name} ingridient={i}/>)
+  return (
+    <div className="ingridientList">
+      <p>Ingridients:</p>
+      {ingridients}
+    </div>
+  )
+}
+
+function Ingridient(props) {
+  const ingridient = props.ingridient;
+  return (
+    <li key={ingridient.name}>{ingridient.name} - {ingridient.quantity} {ingridient.unit}</li>
+  );
+}
+
 
 export { Recipe };
